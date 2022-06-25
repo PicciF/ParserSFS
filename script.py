a92f83a6f45c8b0ac9d72189c19d6f294df0937e
PicciF <marco.picci10@gmail.com>
2022-06-26 01:08:45 +0200

    fixed comments

--- script.py
+++ script.py
@@ -1,15 +1,14 @@
 #parser di file .sfs 
+#TO-DO add argument
 #TO-DO rinominare in modo più sensato le variabili
 from difflib import SequenceMatcher
-import logging
 import rapidfuzz
-from email.errors import InvalidMultipartContentTransferEncodingDefect
+
 ID = 0
 SFS = 1
 POSITION = 2
 LENGTH = 3
 
-#funzione che stampa tutte le sfs tra due posizioni
 def inTheMeaddle(lista, a, b):
     ris = []
     allPosition = get(lista, POSITION)
@@ -18,7 +17,6 @@ def inTheMeaddle(lista, a, b):
             ris.append(lista[cont])
     return ris
 
-#funzione che stampa lunghezza e posizione data una specifica SFS
 def getFromSFS(lista, sfs):
     allSFS = get(lista, SFS)
     for cont in range(0, len(allSFS)):
@@ -33,12 +31,12 @@ def menu():
     print("5. Stampa posizione e lunghezza di una specifica sfs")
     print("6. Stampa SFS fra due posizioni")
     print("7. Stampa le read unite e pulite")
+    print("8. Clusterizza")
+    print("9. Genera rappresentanti")
+    print("10. Esci")
     scelta = int(input("Inserisci la scelta "))
     return scelta
 
-#funzione che permette di ritornare cio che si vuole
-#sfs, posizione o lunghezza
-#fare una funzione per ognuna di essa creava ripetizione inutile di codice
 def get(lista, x):
     ris = []
     for l in lista:
@@ -46,16 +44,13 @@ def get(lista, x):
         print(l[x])
     return ris
 
-#ogni funzione restituisce una lista
-#per far si che sia una stampa più compresibile 
-#faccio un ciclo, per non ripetere codice ho creato questa funzione
 def printList(lista):
     for l in lista:
         print(l)
 def getRead(lista):
     ris = []
     app = []
-    #inserire inizializzazione adeguata cosi non prende il primo della prima read
+    #TO-DO inserire inizializzazione adeguata cosi non prende il primo della prima read
     r = []
     id = lista[0][0]
     prima = True
@@ -72,7 +67,6 @@ def getRead(lista):
             id = read[0]
             
             prima = True
-            #per evitare di perdere/avere liste vuote in caso di singola sfs per read
             if(len(app)==0):
                 ris.append([r])
             else:
@@ -80,7 +74,7 @@ def getRead(lista):
             r = read
             app = []
     return ris
-#rimozione dell'asterisco 
+
 def sostitutionAst(lista):
     idRead = "" 
     for read in lista:
@@ -127,7 +121,6 @@ def fusione(sfs, posizioni):
                 else:
                     contt = []
                     cont = 0
-                    #il for serve perche se sono diversi smette l'iterazione
                     for index in range (0, len(prima)):
                         for i in range (0, len(ultima)):
                             while prima[index] == ultima[i] and i<len(ultima)-1:
@@ -136,7 +129,6 @@ def fusione(sfs, posizioni):
                         contt.append(cont)
                     common = min(contt)     
                     str = prima + ultima[common+1:]
-                    #si puo salvare solo la prima in quanto poi è unito
                     app.append(primaPosizione)
                     app.append(ultimaPosizione)
                     posizioniRis.append(app)
@@ -147,51 +139,31 @@ def fusione(sfs, posizioni):
     
 def mergeRead(lista):
     finale = []
-    nonOver = []
-    nonOverPosizioni = []
     readOv = []
-    readPositionOv = []
-
-    #print((lista[34][0][1])) 
-    # da errore perche essendo un solo elemento non crea una 
-    # lista di liste
-    # ah no questa print crea una lista vuota e quindi il patatrac
-    # controllare dove creo la lista, probabile rimane inizializzata una var
-    # probabile fix basta metttere un controllo se read lunga 1 mettere quella
-    
+    readPositionOv = []    
     sfsRead= []
     sfsReadPos = []
     
     for read in lista:
-        
-        
         sfs = [read[0][POSITION], read[0][LENGTH], read[0][SFS]]
-         
-        
         singoloOv = []
         singolaPositionOv = []
         gruppoOv = []
         gruppoPositionOv = []
         risultofinale = []
         posizioniRisultofinale =[]
-        #qui ce la prima sfs
         
         for cont in range(1, len(read)):
-            #qui entro se trovo due stringhe che sono in overleap
-           
             if int(read[cont][POSITION]) + int(read[cont][LENGTH]) > int(sfs[0]):
+                #there is a overlap
                 over = True
                 posizione = int(sfs[0])
                 singoloOv.append(sfs[2])
                 singolaPositionOv.append(sfs[0])
                 sfs[0] = read[cont][POSITION]
                 sfs[1] = posizione + int(sfs[1]) - int(read[cont][POSITION])
-                sfs[2] = read[cont][SFS]
-
-                
-            else:
-                #prendere sfs non overlap
-                
+                sfs[2] = read[cont][SFS]     
+            else: 
                 if(over==True):
                     singoloOv.append(sfs[2])
                     singolaPositionOv.append(sfs[0])
@@ -199,13 +171,8 @@ def mergeRead(lista):
                 sfs[0] = read[cont][POSITION]
                 sfs[1] = read[cont][LENGTH]
                 sfs[2] = read[cont][SFS]
-                
-                app = [read[cont]]
-
                 risultofinale.append(read[cont][SFS])
-                posizioniRisultofinale.append(read[cont][POSITION])
-                
-                
+                posizioniRisultofinale.append(read[cont][POSITION]) 
                 gruppoOv.append(singoloOv)
                 singoloOv = []
                 gruppoPositionOv.append(singolaPositionOv)
@@ -218,36 +185,15 @@ def mergeRead(lista):
                 gruppoOv.append(singoloOv)
                 singoloOv = []
                 gruppoPositionOv.append(singolaPositionOv)
-                singolaPositionOv = []
-            #applicata modifica con quadre
-            
+                singolaPositionOv = []    
         sfsReadPos.append([posizioniRisultofinale])
         sfsRead.append(risultofinale)
-        
-       
-        #nonOver.append(sfsRead)
-        
-        
-        
-        #nonOverPosizioni.append(sfsReadPos)
-        #if len(sfsReadPos)==0:
-        #    nonOverPosizioni.append([sfsReadPos])
-        #else:
-        #    nonOverPosizioni.append(sfsReadPos)
-        
         readOv.append(gruppoOv)
         readPositionOv.append(gruppoPositionOv)
-    
-   
     sfsUnite, posizioni = fusione(readOv, readPositionOv)
-    cc = 0
-    #metto in listone le sfs non in over
- 
+    
     f = []
-    #
-   
     for i in range(0, len(sfsUnite)):
-        #print(idList[i], " ", i)
         print("Sto processando una nuova read ", i)
         if not len(sfsUnite[i])==0:
             #TO-DO create method to set dim dynamically
@@ -266,39 +212,21 @@ def mergeRead(lista):
                 dim = (int(posizioni[i][0][0])+  int(posizioni[i][0][1])) 
             '''
             dim = 10000
-            
-            finale = ["*"] * dim
-            
-            for x in range(0, len(sfsUnite[i])):
-               
-                cont = int(posizioni[i][x][0])
+            finale = ["*"] * dim 
 
+            for x in range(0, len(sfsUnite[i])):   
+                cont = int(posizioni[i][x][0])
                 for c in sfsUnite[i][x]:
                     finale[cont] = c
                     cont+=1
-                #qui arriva giusto
-        
-        
-        
-        
         for j in range(0, len(sfsRead[i])):
             cont = int(sfsReadPos[i][0][j]) - 1        
-            #print("lunghezza ", len(finale))       
-            #print(idList[i])
             for c in sfsRead[i][j]:    
-                #da un errore qui, non so il perchè
                 finale[cont] = c
                 cont+=1
         
         f.append(finale)
 
-    
-  
-   
-      
-        
-                
-    
     return f
       
 def formatting(lista):
@@ -310,23 +238,13 @@ def formatting(lista):
     ris = sostitutionAst(ris)
     return ris   
  
-
-
-
 lista = []
-#/home/picci/PingPong/asgal-wd/solution_batch_0.sfs
+
 file = open("/home/picci/PingPong/asgal-wd/solution_batch_0.sfs", 'r')
 
 for single in file:
     lista.append(single)
 file.close()
-
-#rimuovo la stringa iniziale 
-#lista[0] = lista[0][36:]
-
-#creo una lista(ogni riga) di lista(ogni campo della riga) 
-#lis = formatting(lista[:10]) utile per debug
-#invece di avere tutte le righe, così è più capibile 
 lis = formatting(lista)
 
 idList = []
@@ -412,7 +330,6 @@ def similarita():
         
     file.close()
             
-    #print((cluster))    
                 
 def indicizza():
     file = open("cluster.txt", 'r')
@@ -422,7 +339,7 @@ def indicizza():
     peso = []
     apppeso = []
     for row in file:
-        #pulisco il file e prendo sfs e peso
+        #clean file and get sfs and weight
         cluster = row.split(" ") 
         cluster = cluster[4:]
         for c in cluster:
@@ -437,83 +354,67 @@ def indicizza():
         peso.append(apppeso)
         appsfs = []
         apppeso = []
-        
-        #maxi = max(peso)
-        #rappresentanti.append(sfs[peso.index(maxi)])
-        #print(rappresentanti)
-        #funziona solo se ce exit dopo uno, va 
-        #messa una lista di appoggio in modo tale che sfs e peso diventi una lista di liste
-    #determino le sfs rappresentati per ogni cluster
+    #I determine the sfs representatives for each cluster
     file.close()
     for i in range(0, len(peso)):
         maxi = max(peso[i])
         rappresentanti.append(sfs[i][peso[i].index(maxi)])
     clusterDeleted = []
-    #rimozione e salvataggio del numero di cluster eliminato
+    #removing and saving the number of clusters deleted
     for r in rappresentanti:
         if len(r)<15:
             clusterDeleted.append(rappresentanti.index(r))
             rappresentanti.remove(r)
     fileFasta = open("rappresentanti.fastq", 'w')
     for i in range(0, len(rappresentanti)):
+        fileFasta.write(">"+ str(i+1))
+        fileFasta.write("\n")
         fileFasta.write(rappresentanti[i])
         fileFasta.write("\n")
     
     
     
-    fileFasta.close()
-
-    
-        
-        
-                      
-        
-        
+    fileFasta.close()   
 
 scelta = menu()
 
-#idList = getIdSFS()
 
-#personalmente userei uno switch ma è presente solo
-#nella versione 3.10 di python e non so se si può usare
-#quindi ora inserisci un semplice if
 
-while(scelta != 8):
+#if we use python 3.10 we could use switch statement
+#so for now use simply if statement
+
+while(scelta != 11):
     if scelta == 1:
-        #stampo tutte le sfs
+        #print all sfs
         sfs = get(lis, SFS)
         printList(sfs)
     if scelta == 2:
-        #stampo tutte le posizioni
+        #print all positions
         sfs = get(lis, POSITION)
         printList(sfs)
     if scelta == 3:
-        #stampo tutte le lunghezze
+        #print all lenghts
         sfs = get(lis, LENGTH)
         printList(sfs)
     if scelta == 4:
-        #stampo tutti gli id
+        #print all id
         sfs = get(lis, ID)
         printList(sfs)
     if scelta == 5:
-        #stampo posizione e lunghezza da una specifica sfs
+        #print position and length from specif sfs
         sfsUtente = input("Inserisci la sfs di cui vuoi posizione e lunghezza: ")
-        #converto in upper case in caso l'utente abbia inserito i caratteri in minuscolo
         sfsUtente = sfsUtente.upper() 
         posizione, lunghezza = getFromSFS(lis, sfsUtente)
         print("la posizione della sfs inserita è: " + posizione)
         print("La lunghezza della sfs inserita è: " + lunghezza)
     if scelta == 6:
-        #inserimento del range di posizione
         a = int(input("Inserisci la posizione minima compresa: "))
         b = int(input("Inserisci la posizione massima compresa: "))
         sfs = inTheMeaddle(lis, a, b)
         printList(sfs)
     if scelta == 7:
-        #lista di liste in cui ho ogni posizione una read contentente tutte le sequence
+        #list of lists, in which for each position have a read, in which have all sequences
         read = getRead(lis)
-        
-        #un lista contente tutte le read complete
         cont = 0
         read = mergeRead(read)
         stringa = ""
@@ -525,46 +426,19 @@ while(scelta != 8):
                 stringa += x  
             ls.append(stringa)
         
-        #ritorna la stringa unificata per ogni read
-        
+        #return unified string 
         file = open("sequenza.txt", 'w')
        
         for i in range(0, len(read)):
             file.write(idList[i])
             file.write("\n")
             file.write("".join(read[i]))
-            file.write("\n")
-        file.close()
-    if scelta == 9:
-        #\\wsl$\Ubuntu\home\picci\PingPong\example
-        #funzione di controllo
-        #prendo il file con le read complete e lo riscrivo
-        #controllo che ci siano tutte
-        file = open("child.fq", 'r')
-        faile = []
-        cont = 0
-        for cas in file:
-            faile.append(cas)
-        for i in faile:
-            if i[1:].rstrip("\n") in idList:
-               cont+=1
-        okFile = False
-        if cont == len(idList):
-            okFile = True
-        readComplete = []
-        idFile = []
-        for c in range(0, len(faile)):
-            r = faile[c]
-            if r[1:].rstrip("\n") in idList:
-                idFile.append(r)
-                readComplete.append(faile[c+1])
+            file.write("\n") 
         file.close()    
-        #id list non e in ordine come nel file di read complete
-        new = open("verifica.txt", 'w')
-        for i in range(0, len(readComplete)):
-            new.write(idFile[i])
-            new.write("".join(readComplete[i]))
-        new.close()  
+    if scelta==8:
+        similarita()
+    if scelta==9:
+        indicizza()
     if scelta==10:
         file = open('sequenza.txt', 'r')
         fileString = ""
@@ -574,12 +448,5 @@ while(scelta != 8):
 
         test = fileString.split('*')     
         print(list(filter(None,test)))
-    if scelta==11:
-        similarita()
-    if scelta==12:
-        indicizza()
-    
-        
-
 
     scelta = menu()
